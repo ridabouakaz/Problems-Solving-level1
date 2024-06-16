630f828ee90a51048b306bd74afd9ff051d467ce
ridabouakaz <bouakaz99@gmail.com>
2024-06-16 02:18:33 +0100

    Implement-a-function-which-convert-the-given-boolean-value-into-its-string-representation

---
+++ problemnumber6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include <string>
+#include<array>
+using namespace std;
+string divisible(bool b) {
+
+	return  b ? "true" : "false";
+}
+int main() {
+	cout << divisible(0);
+
+	return 0;
+}
