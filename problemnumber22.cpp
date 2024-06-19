f677f00349bbf11b9d7e23ca5d3cbab5c7536e3a
Ridha bouakkaz <154628150+ridabouakaz@users.noreply.github.com>
2024-06-19 03:05:11 +0100

    Update problemnumber22.cpp

--- problemnumber22.cpp
+++ problemnumber22.cpp
@@ -3,7 +3,6 @@
 #include<array>
 #include<vector>
 using namespace std;
-// function covert lowercase to uppercase each and uppercase letter becomes lowercase 
 int  calculgallons(int gallon, int mill) {
 	int result = gallon * 25;
 
