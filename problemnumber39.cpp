4d1f4595a74f84452baa3f3f579561fc1efe2622
ridabouakaz <bouakaz99@gmail.com>
2024-06-21 03:49:41 +0100

    update-solution39

--- problemnumber39.cpp
+++ problemnumber39.cpp
@@ -4,17 +4,14 @@
 #include<vector>
 using namespace std;
 // function return an array of all integers between the input parameters
-int  integersbetweeninput(int num1, int num2) {
-
+int main(int num1, int num2) {
+	vector<int> list;
 	for (int i = num1; i <= num2; i++)
 	{
 
-		cout << i;
+		list.push_back(i);
+		
 	}
-	return 0;
-}
-int main() {
-	integersbetweeninput(1, 4);
-	return 0;
+	return list;
 }
 /*Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.*/
