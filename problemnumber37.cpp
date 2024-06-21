de410ef319a864b1cce2a1607fb51880d0d16472
ridabouakaz <bouakaz99@gmail.com>
2024-06-21 02:43:57 +0100

    solution-number37

---
+++ problemnumber37.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <string>
+#include<array>
+#include<vector>
+using namespace std;
+// function  takes a number and returns the corresponding ASCII
+int       numberreturnsASCII(int nume) {
+
+	cout << char(nume);
+	return 0;
+}
+int main() {
+	numberreturnsASCII(97);
+	return 0;
+}
+/*Write a function which takes a number and returns the corresponding ASCII char for that value*/
