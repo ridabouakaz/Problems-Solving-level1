2a2022f408f6e218e346d9c88e8d842da8baa930
ridabouakaz <bouakaz99@gmail.com>
2024-06-29 05:33:07 +0100

    solution-number59

---
+++ problemnumber59.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <string>
+#include<array>
+#include<vector>
+using namespace std;
+
+// fixed bug in code
+string howManyDalmatians(int number) {
+	vector<string>dogs = { "Hardly any", "More than a handful!","Woah that's a lot of dogs!", "101 DALMATIONS!!!" };
+	return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number <= 100 ? dogs[2] : dogs[3];
+}
+int main() {
+	howManyDalmatians(2);
+	return 0;
+}
+/*Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+The number of dogs will always be a number and there will always be at least 1 dog.*/
