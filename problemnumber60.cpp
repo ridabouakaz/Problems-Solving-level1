a7ceaf1b0ce501ec448e94e66244793e6566bdae
ridabouakaz <bouakaz99@gmail.com>
2024-07-02 05:17:14 +0100

    solution-number60

---
+++ problemnumber60.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <string>
+#include<vector>
+using namespace std;
+
+//  function convert int to string
+int number_to_string_function(int number1) {
+	cout << to_string(number1);
+	return 0;
+}
+int main() {
+	number_to_string_function(67);
+	return 0;
+}
+/*We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?*/
