cbe0d603e1420c6360f6da9d9f18f8bf080ba0f2
ridabouakaz <bouakaz99@gmail.com>
2024-07-02 18:04:08 +0100

    solution-number63

---
+++ problemnumber63.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<vector>
+bool set_alarm(const bool& employed, const bool& vacation)
+{
+	return (employed && !vacation);
+}/*Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples*/
