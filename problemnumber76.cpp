7f0d066d22e09dfc586bbd835e6a28d61c59e1cc
ridabouakaz <bouakaz99@gmail.com>
2024-07-04 17:50:13 +0100

    solution-number76

---
+++ problemnumber76.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+
+//  function It counts how many years ago the father was twice as old as his son (or in how many years he will be twice as old)
+int twice_as_old(int dad, int son) {
+	return son * 2 - dad > 0 ? son * 2 - dad : (son * 2 - dad) * -1;
+}
+int main() {
+	cout << twice_as_old(36, 7);
+	return 0;
+}
+/*Your function takes two arguments:
+
+	current father's age (years)
+	current age of his son (years)
+
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+*/
